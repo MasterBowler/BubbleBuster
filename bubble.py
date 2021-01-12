73d1a18fd05990e5be48246d20cf130dcda829ba
Manolache Mihai <40718703+MasterBowler@users.noreply.github.com>
2021-01-12 20:07:18 +0200

    bubble class
    implements a bubble

--- bubble.py
+++ bubble.py
@@ -9,8 +9,8 @@ class Bubble:
         self.moving = moving
 
     def reinitialize(self):
-        self.x = SCREEN_WIDTH / 2 - TILE_WIDTH / 2
-        self.y = SCREEN_HEIGHT - TILE_WIDTH
+        self.x = SCREEN_WIDTH / 2 - BUBBLE_WIDTH / 2
+        self.y = SCREEN_HEIGHT - BUBBLE_WIDTH
         self.speed = 500
         self.angle = 90
         self.bubble_color = random.randint(0,len(COLORS)-1)
