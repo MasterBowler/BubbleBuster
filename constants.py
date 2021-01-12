1eac1488d0056e91c20351f7a65727d753472837
Manolache Mihai <40718703+MasterBowler@users.noreply.github.com>
2021-01-12 19:53:58 +0200

    a few constants

---
+++ constants.py
@@ -0,0 +1,12 @@
+import random
+import os, sys
+import math
+import pygame
+from pygame.locals import *
+
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 640
+COLUMNS = 12
+ROWS = 19
+COLORS = ['red','green','blue','gray']
+TILE_WIDTH = 32
