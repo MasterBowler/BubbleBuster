3168ae32db8b8b1ab963c3852e521a8b513960fe
Manolache Mihai <40718703+MasterBowler@users.noreply.github.com>
2021-01-13 07:47:27 +0200

    added levels count

--- constants.py
+++ constants.py
@@ -7,6 +7,7 @@ from pygame.locals import *
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 640
 COLUMNS = 12
-ROWS = 19
-COLORS = ['red','green','blue','gray']
+ROWS = 18
+COLORS = ["red", "green", "blue", "gray"]
 BUBBLE_WIDTH = 32
+LEVELS = 3
