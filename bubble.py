12e4edd938d2e0a68280441dfae43af77a4a1611
Manolache Mihai <40718703+MasterBowler@users.noreply.github.com>
2021-01-13 07:48:06 +0200

    added neighbours direction arrays

--- bubble.py
+++ bubble.py
@@ -1,5 +1,10 @@
 from constants import *
+
+
 class Bubble:
+    even_row_neighbours = [[-1, -1], [-1, 0], [0, 1], [1, 0], [1, -1], [0, -1]]
+    odd_row_neighbours = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [0, -1]]
+
     def __init__(self, x, y, speed, angle, color, moving):
         self.x = x
         self.y = y
@@ -7,11 +12,13 @@ class Bubble:
         self.angle = angle
         self.bubble_color = color
         self.moving = moving
+        self.to_remove = False
 
-    def reinitialize(self):
-        self.x = SCREEN_WIDTH / 2 - BUBBLE_WIDTH / 2
-        self.y = SCREEN_HEIGHT - BUBBLE_WIDTH
-        self.speed = 500
-        self.angle = 90
-        self.bubble_color = random.randint(0,len(COLORS)-1)
-        self.moving = False
+    def reinitialize(self, x, y, speed, angle, color, moving):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.angle = angle
+        self.bubble_color = color
+        self.moving = moving
+        self.to_remove = False
